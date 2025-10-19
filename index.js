c350cf16b1aefb54fa7ee981de85abd69a5a2be6
Antareep Sarkar <sarkar.antareep.9035@gmail.com>
2025-10-19 12:50:44 +0530

    Made a separate file for conversion functions

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-import { PDFDocument } from 'pdf-lib'
-
 function show(){
      const img = document.createElement("img");
      img.src = URL.createObjectURL(document.getElementById("file_inp").files[0]);
      document.getElementById('body').appendChild(img);
 }
 
-async function ImageToPDF(imgObjURL){
-     const imgBytes = await imgObjURL.arrayBuffer();
-     const pdfDoc = await PDFDocument.create();
-     const pngImage = await pdfDoc.embedPng(imgBytes);
-     const page = pdfDoc.addPage();
-     page.drawImage(pngImage);
-     const pdfBytes = await pdfDoc.save();
-     return pdfBytes;
+function Convert(){
+
 }
 
-function Convert(){
-     
-}
 function DownloadFile(){
      browser.downloads.download({
      url: downloadUrl,
